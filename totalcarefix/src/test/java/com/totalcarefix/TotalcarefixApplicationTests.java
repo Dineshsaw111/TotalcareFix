06c0b6516ad884268e36d7ffcc13ca382f640cf2
Dinesh Saw <dinesh.saw@vcontractor.co.za>
2024-03-06 16:57:42 +0530

    backend added

---
+++ totalcarefix/src/test/java/com/totalcarefix/TotalcarefixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totalcarefix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TotalcarefixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
