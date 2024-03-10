f54b494d08c0e6cd2fca4646b392d4dade239a0b
nishant-taletiya <nishanttaletiya111111@gmail.com>
2024-03-10 18:24:10 +0530

    new commit

--- src/main/java/com/totalcarefix/HomeController.java
+++ src/main/java/com/totalcarefix/HomeController.java
@@ -10,6 +10,6 @@ public class HomeController {
     @ResponseBody
     public String home()
     {
-        return "Welcome To our Website TotalCareFix 1";
+        return "Welcome To our Website TotalCareFix";
     }
 }
