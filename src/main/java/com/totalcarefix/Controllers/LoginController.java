20afe76a361c8bcd3b2b7efd5bbb2b7ef06907d1
dienshsaw <kumardineswh1999@gmail.com>
2024-05-12 16:14:42 +0530

    Login-jwt-token-genearation

--- src/main/java/com/totalcarefix/Controllers/LoginController.java
+++ src/main/java/com/totalcarefix/Controllers/LoginController.java
@@ -1,29 +1,44 @@
 package com.totalcarefix.Controllers;
 
+import com.totalcarefix.DTO.LoginDto;
+import com.totalcarefix.Entities.Users;
+import com.totalcarefix.Repos.UsersRepo;
+import com.totalcarefix.Services.LoginService;
+import com.totalcarefix.Services.UsersService;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
 
     private static final String SECRET_KEY = "5R@hP2A+gQkzXK9vS4M*E7jWdGdF5aJd";
     private static final long EXPIRATION_TIME = 864_000_000; // 10 days in milliseconds
 
     @PostMapping("/auth")
     public Map<String, String> generateToken(@RequestBody Map<String, String> request) {
-        // Extract email from the request body
+
+
         String email = request.get("email");
+       LoginDto loginDto= loginService.getToken(email);
+
 
         // Calculate token expiration time
         Date expirationDate = new Date(System.currentTimeMillis() + EXPIRATION_TIME);
 
-        // Create a JWT token with the email as the subject
+        // Create a JWT token
         String token = Jwts.builder()
                 .setSubject(email)
                 .setExpiration(expirationDate)
@@ -32,7 +47,10 @@ public class LoginController {
 
         // Create a response object containing the token
         Map<String, String> response = new HashMap<>();
-        response.put("token", token);
+         response.put("token", token);
+         response.put("email",loginDto.getEmail());
+         response.put("role",loginDto.getRole());
+
 
         return response;
     }
