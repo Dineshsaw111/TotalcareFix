39671db0277ca1de374e326dba49fc1af128b824
VF-ROOT\talen001 <nishant.taletiya@vcontractor.co.za>
2024-05-18 14:10:56 +0530

    implement of google token varification

--- src/main/java/com/totalcarefix/config/FilterConfig.java
+++ src/main/java/com/totalcarefix/config/FilterConfig.java
@@ -1,25 +1,27 @@
-package com.totalcarefix.config;//package com.totalcarefix.config;
+package com.totalcarefix.config;
 
-import com.totalcarefix.filer.TokenVerificationFilter;
+import com.totalcarefix.filter.TokenVerificationFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.core.Ordered;
 
 @Configuration
 public class FilterConfig {
 
     @Bean
-    public FilterRegistrationBean<TokenVerificationFilter> tokenVerificationFilter() {
+    public FilterRegistrationBean<TokenVerificationFilter> tokenVerificationFilter(JwtDecoder jwtDecoder) {
         FilterRegistrationBean<TokenVerificationFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new TokenVerificationFilter());
 
-        // Exclude the endpoint for generating token from token verification
+        TokenVerificationFilter filter = new TokenVerificationFilter();
+        filter.setJwtDecoder(jwtDecoder);
+
+        registrationBean.setFilter(filter);
         registrationBean.addUrlPatterns("/*");
-        registrationBean.addUrlPatterns("/login/auth"); // Exclude this endpoint
-        registrationBean.addUrlPatterns("/cities/getAllUserCities");
-        registrationBean.addUrlPatterns("/register");
+//        registrationBean.addUrlPatterns("/cities/getAllUserCities");
         registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
         return registrationBean;
     }
 }
